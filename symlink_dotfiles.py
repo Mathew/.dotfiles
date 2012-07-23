e83074c66f27ddb08e8ab5942cbe5cad2fc7baf3
Mathew Taylor <mat@mathewtaylor.co.uk>
2012-07-23 14:07:03 +0100

    add ST2 dotfiles

--- symlink_dotfiles.py
+++ symlink_dotfiles.py
@@ -1,8 +1,10 @@
 """
-Symlinks files in the files_to_link list from the 
+Symlinks files in the files_to_link list from the
 dotfiles dir to the users home dir
 """
-import os, sys
+import os
+import shutil
+import sys
 
 files_to_link = ['gitconfig', 'profile', 'vimrc', 'vim']
 #get home dir
@@ -11,13 +13,25 @@ home_dir = os.path.expanduser('~')
 dotfiles_path = sys.path[0]
 
 for file in files_to_link:
-    #for each file, check if it exists, if so do nothing since 
+    #for each file, check if it exists, if so do nothing since
     #we don't want to destroy someone's files
-    if os.path.exists(home_dir+'/.'+file):
+    if os.path.exists(home_dir + '/.' + file):
         print '%s already exists' % file
     else:
         #print message to display link, then link
         print 'linking %s/.%s to %s/%s' % (home_dir,
-            file, dotfiles_path, file) 
+            file, dotfiles_path, file)
         os.symlink('%s/%s' % (dotfiles_path, file),
             '%s/.%s' % (home_dir, file))
+
+# sublime text setting sit in it's own directory so we'll do this
+# seperately
+sublime_path = '{0}/Library/Application Support/Sublime Text 2/Packages/User'.format(home_dir)
+
+#delete then copy, st2 doesn't work with a symlink
+try:
+    shutil.rmtree(sublime_path)
+except OSError, e:
+    print e
+shutil.copytree("{0}/{1}".format(dotfiles_path, 'sublime_settings'),
+    sublime_path)
